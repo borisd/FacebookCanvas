a6747edfe17d89e0721d4b4b66c169d37c4f0822
Boris Dinkevich <Boris@dinkevich.com>
2012-03-25 18:22:24 +0200

    Save user's access token

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,16 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :trackable, :validatable, :omniauthable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password
+  attr_accessible :email, :password, :access_token
 
   def self.find_for_facebook_oauth(access_token, signed_in_resource=nil)
     data = access_token.extra.raw_info
     if user = User.where(:email => data.email).first
       user
     else # Create a user with a stub password. 
-      User.create!(:email => data.email, :password => Devise.friendly_token[0,20]) 
+      User.create!(:email => data.email, 
+                   :password => Devise.friendly_token[0,20],
+                   :access_token => access_token.credentials.token)
     end
   end
 
@@ -17,6 +19,7 @@ class User < ActiveRecord::Base
     super.tap do |user|
       if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
         user.email = data["email"]
+        user.access_token = session["devise.facebook_data"]["credentials"]["token"]
       end
     end
   end
