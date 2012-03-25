e77220fca59ef45d75e710456e2ecebe28fc8ed1
Boris Dinkevich <Boris@dinkevich.com>
2012-03-25 16:58:54 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fbstream::Application.config.secret_token = 'd617ef6debda86d5090bd75e5fabe40103d8fb7afe8b281e3c81032bbd557ae85a812e9e38ddac7d467b37bbebe110424fbdee929597e64f4ccf3cb06ebbd6ba'
